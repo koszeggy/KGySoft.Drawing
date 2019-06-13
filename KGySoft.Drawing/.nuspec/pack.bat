62b04c84dcfcef009180df6403664291eb5a58f7
György Kőszeg <koszeggy@yahoo.com>
2019-06-13 15:52:35 +0200

    Releasing 4.6.0

--- KGySoft.Drawing/.nuspec/pack.bat
+++ KGySoft.Drawing/.nuspec/pack.bat
@@ -1,1 +1,1 @@
-@nuget pack -properties Configuration=Release KGySoft.CoreLibraries.nuspec
+@nuget pack -properties Configuration=Release KGySoft.Drawing.nuspec
--- KGySoft.Drawing/Properties/AssemblyInfo.cs
+++ KGySoft.Drawing/Properties/AssemblyInfo.cs
@@ -8,12 +8,16 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Drawing")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("KGySoft.Drawing")]
+[assembly: AssemblyDescription("KGy SOFT Drawing Libraries")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyCompany("KGy SOFT")]
 [assembly: AssemblyProduct("KGy SOFT Drawing")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © KGy SOFT. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,20 +29,11 @@ using System.Security;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c0a31975-3d90-4d77-980b-f64c44f0e3d2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
+[assembly: CLSCompliant(true)]
+
 [assembly: AssemblyVersion("4.6.0.0")]
 [assembly: AssemblyFileVersion("4.6.0.0")]
 [assembly: AssemblyInformationalVersion("4.6.0")]
-
-[assembly: CLSCompliant(true)]
 
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: InternalsVisibleTo("KGySoft.Drawing.UnitTest, PublicKey=00240000048000009400000006020000002400005253413100040000010001003928BADFAA8C02789566AB7AC64A59DCDE30B798589A68EF92CBB04C9DED3FCBFE41F644D424DCF82F8A13F9148D45EE15785450318388E01AA8C4CF645E81C772E39DCA0D14B33CF48167B70F5C34A0E7B763141ED3AFDDAD0373D9FCD2E153E78D201C5C4EB61DBBD586EC6291EABFBE11879865C3776088605FA8820387C2")]
