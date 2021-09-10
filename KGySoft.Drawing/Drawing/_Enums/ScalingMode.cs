c6e94d038ab5a04401a8a91bb18c2b1745bf4936
György Kőszeg <koszeggy@gmail.com>
2021-09-10 19:10:39 +0200

    Updating file headers

--- KGySoft.Drawing/Drawing/_Enums/ScalingMode.cs
+++ KGySoft.Drawing/Drawing/_Enums/ScalingMode.cs
@@ -1,4 +1,19 @@
-﻿namespace KGySoft.Drawing
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: ScalingMode.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2021 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+namespace KGySoft.Drawing
 {
     /// <summary>
     /// Represents the scaling mode when an image needs to be resized.
