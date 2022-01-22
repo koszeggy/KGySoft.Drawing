0c1dedaa8ab32c409032c7012592162ccfd5a5a7
György Kőszeg <koszeggy@gmail.com>
2022-01-22 12:41:01 +0100

    Fixing refactoring oversights

--- KGySoft.Drawing/Drawing/Imaging/_BitmapData/_Managed/_1DArrayBased/_SpecificBufferType/ManagedBitmapDataRow64PArgb.cs
+++ KGySoft.Drawing/Drawing/Imaging/_BitmapData/_Managed/_1DArrayBased/_SpecificBufferType/ManagedBitmapDataRow64PArgb.cs
@@ -21,22 +21,21 @@ using System.Runtime.CompilerServices;
 
 namespace KGySoft.Drawing.Imaging
 {
-    internal sealed class ManagedBitmapDataRow64PArgb<T> : ManagedBitmapDataRowBase<T>
-        where T : unmanaged
+    internal sealed class ManagedBitmapDataRow64PArgb : ManagedBitmapDataRowBase<Color64>
     {
         #region Methods
 
         [MethodImpl(MethodImpl.AggressiveInlining)]
-        public override Color32 DoGetColor32(int x) => DoReadRaw<Color64>(x).ToStraight().ToColor32();
+        public override Color32 DoGetColor32(int x) => Row[x].ToStraight().ToColor32();
 
         [MethodImpl(MethodImpl.AggressiveInlining)]
-        public override void DoSetColor32(int x, Color32 c) => DoWriteRaw(x, new Color64(c).ToPremultiplied());
+        public override void DoSetColor32(int x, Color32 c) => Row[x] = new Color64(c).ToPremultiplied();
 
         [MethodImpl(MethodImpl.AggressiveInlining)]
-        public override Color32 DoGetColor32Premultiplied(int x) => DoReadRaw<Color64>(x).ToColor32();
+        public override Color32 DoGetColor32Premultiplied(int x) => Row[x].ToColor32();
 
         [MethodImpl(MethodImpl.AggressiveInlining)]
-        public override void DoSetColor32Premultiplied(int x, Color32 c) => DoWriteRaw(x, new Color64(c));
+        public override void DoSetColor32Premultiplied(int x, Color32 c) => Row[x] = new Color64(c);
 
         #endregion
     }
