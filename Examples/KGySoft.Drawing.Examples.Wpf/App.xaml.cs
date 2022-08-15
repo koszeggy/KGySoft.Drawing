fef453e5eb5dcb7bb303e5e51a44e04c073e8d14
György Kőszeg <koszeggy@gmail.com>
2022-08-15 12:44:14 +0200

    WPF Example: Upgrading to latest version

--- Examples/KGySoft.Drawing.Examples.Wpf/App.xaml.cs
+++ Examples/KGySoft.Drawing.Examples.Wpf/App.xaml.cs
@@ -1,9 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: App.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2022 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
 using System.Windows;
+
+#endregion
 
 namespace KGySoft.Drawing.Examples.Wpf
 {
