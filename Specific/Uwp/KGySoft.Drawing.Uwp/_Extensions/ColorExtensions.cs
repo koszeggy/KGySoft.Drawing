c5631ba91c6dd7cb9935aad80a03e5f6de9806e7
György Kőszeg <koszeggy@gmail.com>
2022-09-10 20:14:09 +0200

    UWP: Adding ColorExtensions

---
+++ Specific/Uwp/KGySoft.Drawing.Uwp/_Extensions/ColorExtensions.cs
@@ -0,0 +1,63 @@
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: ColorExtensions.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2022 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+using Windows.UI;
+
+using KGySoft.Drawing.Imaging;
+
+#endregion
+
+namespace KGySoft.Drawing.Uwp
+{
+    /// <summary>
+    /// Contains extension methods for the <see cref="Color"/> type.
+    /// </summary>
+    public static class ColorExtensions
+    {
+        #region Methods
+
+        /// <summary>
+        /// Converts a <see cref="Color">Windows.UI.Color</see> struct to <see cref="Color32">KGySoft.Drawing.Imaging.Color32</see>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color32 ToColor32(this Color color) => new Color32(color.A, color.R, color.G, color.B);
+
+        /// <summary>
+        /// Converts a <see cref="Color32">KGySoft.Drawing.Imaging.Color32</see> struct to <see cref="Color">Windows.UI.Color</see>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color ToWindowsColor(this Color32 color) => Color.FromArgb(color.A, color.R, color.G, color.B);
+
+        /// <summary>
+        /// Converts a <see cref="Color">Windows.UI.Color</see> struct to <see cref="System.Drawing.Color">System.Drawing.Color</see>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static System.Drawing.Color ToDrawingColor(this Color color) => System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
+
+        /// <summary>
+        /// Converts a <see cref="System.Drawing.Color">System.Drawing.Color</see> struct to <see cref="Color">Windows.UI.Color</see>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color ToWindowsColor(this System.Drawing.Color color) => Color.FromArgb(color.A, color.R, color.G, color.B);
+
+        #endregion
+    }
+}
