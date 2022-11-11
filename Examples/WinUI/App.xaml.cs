19350c4f2e6e01202eeca8a4c14ad8ce9fec6c0f
György Kőszeg <koszeggy@gmail.com>
2022-11-11 22:20:21 +0100

    Upgrading package versions

--- Examples/WinUI/App.xaml.cs
+++ Examples/WinUI/App.xaml.cs
@@ -37,7 +37,7 @@ namespace KGySoft.Drawing.Examples.WinUI
     {
         #region Fields
 
-        private Window window;
+        private Window? window;
 
         #endregion
 
@@ -62,7 +62,7 @@ namespace KGySoft.Drawing.Examples.WinUI
         /// Invoked when the application is launched normally by the end user.  Other entry points
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
+        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs e)
         {
             window = new Window { Title = "KGy SOFT Drawing WinUI Example App" };
@@ -111,20 +111,6 @@ namespace KGySoft.Drawing.Examples.WinUI
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
         }
 
         #endregion
