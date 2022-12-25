0f47948c8a5ce20d876e13b391935d19cb257151
György Kőszeg <koszeggy@gmail.com>
2022-12-25 15:17:41 +0100

    Adding resources

---
+++ Specific/SkiaSharp/KGySoft.Drawing.SkiaSharp/Res.cs
@@ -0,0 +1,115 @@
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: Res.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2022 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+using System;
+using System.Globalization;
+
+using KGySoft.CoreLibraries;
+using KGySoft.Resources;
+
+using SkiaSharp;
+
+#endregion
+
+namespace KGySoft.Drawing.SkiaSharp
+{
+    /// <summary>
+    /// Contains the string resources of the project.
+    /// </summary>
+    internal static class Res
+    {
+        #region Constants
+
+        private const string unavailableResource = "Resource ID not found: {0}";
+        private const string invalidResource = "Resource text is not valid for {0} arguments: {1}";
+
+        #endregion
+
+        #region Fields
+
+        private static readonly DynamicResourceManager resourceManager = new DynamicResourceManager("KGySoft.Drawing.SkiaSharp.Messages", typeof(Res).Assembly)
+        {
+            SafeMode = true,
+            UseLanguageSettings = true,
+        };
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>The bitmap must not be frozen.</summary>
+        internal static string BitmapFrozen => Get("BitmapFrozen");
+
+        /// <summary>The IQuantizer.Initialize method returned a null reference.</summary>
+        internal static string QuantizerInitializeNull => Get("QuantizerInitializeNull");
+
+        /// <summary>Could not perform a callback on the thread of the source bitmap. It can be due to a blocking wait on the returned task or async result, or because there is no running dispatcher.</summary>
+        internal static string DispatcherDeadlock => Get("DispatcherDeadlock");
+
+        /// <summary>The bitmap data has an invalid size.</summary>
+        internal static string InvalidBitmapDataSize => Get("InvalidBitmapDataSize");
+
+        #endregion
+
+        #region Methods
+
+        #region Internal Methods
+
+        /// <summary>Internal Error: {0}</summary>
+        /// <remarks>Use this method to avoid CA1303 for using string literals in internal errors that never supposed to occur.</remarks>
+        internal static string InternalError(string msg) => Get("General_InternalErrorFormat", msg);
+
+        /// <summary>The color type '{0}' and alpha type '{1}' do not represent a valid image info.</summary>
+        internal static string ImageInfoInvalid(SKColorType colorType, SKAlphaType alphaType) => Get("General_ImageInfoInvalidFormat", Enum<SKColorType>.ToString(colorType), Enum<SKAlphaType>.ToString(alphaType));
+
+        #endregion
+
+        #region Private Methods
+
+        private static string Get(string id) => resourceManager.GetString(id, LanguageSettings.DisplayLanguage) ?? String.Format(CultureInfo.InvariantCulture, unavailableResource, id);
+
+        private static string Get(string id, params object?[]? args)
+        {
+            string format = Get(id);
+            return args == null ? format : SafeFormat(format, args);
+        }
+
+        private static string SafeFormat(string format, object?[] args)
+        {
+            try
+            {
+                int i = Array.IndexOf(args, null);
+                if (i >= 0)
+                {
+                    string nullRef = PublicResources.Null;
+                    for (; i < args.Length; i++)
+                        args[i] ??= nullRef;
+                }
+
+                return String.Format(LanguageSettings.FormattingLanguage, format, args);
+            }
+            catch (FormatException)
+            {
+                return String.Format(CultureInfo.InvariantCulture, invalidResource, args.Length, format);
+            }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
