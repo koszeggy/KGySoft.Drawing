c6987e9c14bcb730f4f938415ef1d3dcdbe273d8
György Kőszeg <koszeggy@gmail.com>
2023-05-06 17:39:39 +0200

    Unit tests

--- KGySoft.Drawing.Core.UnitTest/UnitTests/Imaging/ColorExtensionsTest.cs
+++ KGySoft.Drawing.Core.UnitTest/UnitTests/Imaging/ColorExtensionsTest.cs
@@ -15,6 +15,8 @@
 
 #region Usings
 
+using System;
+
 using KGySoft.Drawing.Imaging;
 
 using NUnit.Framework;
@@ -73,6 +75,63 @@ namespace KGySoft.Drawing.UnitTests.Imaging
 
             PColorF pFSrgb = new PColorF(p32);
             Assert.AreEqual(p32, pFSrgb.ToPColor32(false));
+        }
+
+        [Test]
+        public void IsValidTest()
+        {
+            var pc32 = new PColor32(128, 255, 128, 64);
+            Assert.IsFalse(pc32.IsValid);
+
+            PColor32 valid = pc32.Clip();
+            Assert.IsTrue(valid.IsValid);
+            Assert.AreEqual(new PColor32(128, 128, 128, 64), valid);
+
+            var pc64 = new PColor64(32768, 65535, 32768, 16384);
+            Assert.IsFalse(pc64.IsValid);
+
+            PColor64 valid64 = pc64.Clip();
+            Assert.IsTrue(valid64.IsValid);
+            Assert.AreEqual(new PColor64(32768, 32768, 32768, 16384), valid64);
+
+            PColorF pF = new PColorF(0.5f, 1f, 0.5f, 0.25f);
+            Assert.IsFalse(pF.IsValid);
+
+            PColorF pValidF = pF.Clip();
+            Assert.IsTrue(pValidF.IsValid);
+            Assert.AreEqual(new PColorF(0.5f, 0.5f, 0.5f, 0.25f), pValidF);
+
+            pF = new PColorF(0.5f, 1.1f, -0.5f, Single.NaN);
+            Assert.IsFalse(pF.IsValid);
+
+            pValidF = pF.Clip();
+            Assert.IsTrue(pValidF.IsValid);
+            Assert.AreEqual(new PColorF(0.5f, 0.5f, 0f, 0f), pValidF);
+
+            var cF = new ColorF(0.5f, 1.1f, -0.5f, Single.NaN);
+            Assert.IsFalse(pF.IsValid);
+
+            ColorF validF = cF.Clip();
+            Assert.IsTrue(validF.IsValid);
+            Assert.AreEqual(new ColorF(0.5f, 1f, 0f, 0f), validF);
+        }
+
+        [Test]
+        public void TolerantEqualsTest()
+        {
+            Assert.IsTrue(new Color32(1, 2, 3, 4).TolerantEquals(new Color32(2, 3, 4, 5), 1, 1));
+            Assert.IsFalse(new Color32(1, 2, 3, 4).TolerantEquals(new Color32(2, 3, 4, 5), 1, 2));
+            Assert.IsTrue(new Color64(1, 2, 3, 4).TolerantEquals(new Color64(2, 3, 4, 5), 1, 1));
+            Assert.IsFalse(new Color64(1, 2, 3, 4).TolerantEquals(new Color64(2, 3, 4, 5), 1, 2));
+            Assert.IsTrue(new ColorF(0.1f, 0.2f, 0.3f, 0.4f).TolerantEquals(new ColorF(0.2f, 0.3f, 0.4f, 0.5f), 0.15f, 0.1f));
+            Assert.IsFalse(new ColorF(0.1f, 0.2f, 0.3f, 0.4f).TolerantEquals(new ColorF(0.2f, 0.3f, 0.4f, 0.5f), 0.15f, 0.2f));
+
+            Assert.IsTrue(new PColor32(4, 3, 2, 1).TolerantEquals(new PColor32(5, 4, 3, 2), 1, 4));
+            Assert.IsFalse(new PColor32(4, 3, 2, 1).TolerantEquals(new PColor32(5, 4, 3, 2), 1, 5));
+            Assert.IsTrue(new PColor64(4, 3, 2, 1).TolerantEquals(new PColor64(5, 4, 3, 2), 1, 4));
+            Assert.IsFalse(new PColor64(4, 3, 2, 1).TolerantEquals(new PColor64(5, 4, 3, 2), 1, 5));
+            Assert.IsTrue(new PColorF(0.4f, 0.3f, 0.2f, 0.1f).TolerantEquals(new PColorF(0.5f, 0.4f, 0.3f, 0.2f), 0.15f, 0.4f));
+            Assert.IsFalse(new PColorF(0.4f, 0.3f, 0.2f, 0.1f).TolerantEquals(new PColorF(0.5f, 0.4f, 0.3f, 0.2f), 0.15f, 0.5f));
         }
 
         #endregion
