fd992bb84bdd2334e042a95d55f46925bd9c8f83
György Kőszeg <koszeggy@gmail.com>
2023-11-11 17:50:42 +0100

    Drawing.WinUI: Adding new overloads to ColorExtensions

--- Specific/WinUI/KGySoft.Drawing.WinUI/_Extensions/ColorExtensions.cs
+++ Specific/WinUI/KGySoft.Drawing.WinUI/_Extensions/ColorExtensions.cs
@@ -31,20 +31,6 @@ namespace KGySoft.Drawing.WinUI
         #region Methods
 
         /// <summary>
-        /// Converts a <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a> struct to <see cref="Color32">KGySoft.Drawing.Imaging.Color32</see>.
-        /// </summary>
-        /// <param name="color">The source color.</param>
-        /// <returns>The result of the conversion.</returns>
-        public static Color32 ToColor32(this Color color) => new (color.A, color.R, color.G, color.B);
-
-        /// <summary>
-        /// Converts a <see cref="Color32">KGySoft.Drawing.Imaging.Color32</see> struct to <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a>.
-        /// </summary>
-        /// <param name="color">The source color.</param>
-        /// <returns>The result of the conversion.</returns>
-        public static Color ToWindowsColor(this Color32 color) => Color.FromArgb(color.A, color.R, color.G, color.B);
-
-        /// <summary>
         /// Converts a <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a> struct to <see cref="System.Drawing.Color">System.Drawing.Color</see>.
         /// </summary>
         /// <param name="color">The source color.</param>
@@ -52,11 +38,95 @@ namespace KGySoft.Drawing.WinUI
         public static System.Drawing.Color ToDrawingColor(this Color color) => System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
 
         /// <summary>
+        /// Converts a <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a> struct to <see cref="Color32"/>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color32 ToColor32(this Color color) => new (color.A, color.R, color.G, color.B);
+
+        /// <summary>
+        /// Converts a <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a> struct to <see cref="PColor32"/>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static PColor32 ToPColor32(this Color color) => color.ToColor32().ToPColor32();
+
+        /// <summary>
+        /// Converts a <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a> struct to <see cref="Color64"/>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color64 ToColor64(this Color color) => color.ToColor32().ToColor64();
+
+        /// <summary>
+        /// Converts a <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a> struct to <see cref="PColor64"/>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static PColor64 ToPColor64(this Color color) => color.ToColor32().ToPColor64();
+
+        /// <summary>
+        /// Converts a <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a> struct to <see cref="ColorF"/>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static ColorF ToColorF(this Color color) => color.ToColor32().ToColorF();
+
+        /// <summary>
+        /// Converts a <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a> struct to <see cref="PColorF"/>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static PColorF ToPColorF(this Color color) => color.ToColor32().ToPColorF();
+
+        /// <summary>
         /// Converts a <see cref="System.Drawing.Color">System.Drawing.Color</see> struct to <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a>.
         /// </summary>
         /// <param name="color">The source color.</param>
         /// <returns>The result of the conversion.</returns>
         public static Color ToWindowsColor(this System.Drawing.Color color) => Color.FromArgb(color.A, color.R, color.G, color.B);
+
+        /// <summary>
+        /// Converts a <see cref="Color32"/> struct to <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color ToWindowsColor(this Color32 color) => Color.FromArgb(color.A, color.R, color.G, color.B);
+
+        /// <summary>
+        /// Converts a <see cref="PColor32"/> struct to <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color ToWindowsColor(this PColor32 color) => color.ToColor32().ToWindowsColor();
+
+        /// <summary>
+        /// Converts a <see cref="Color64"/> struct to <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color ToWindowsColor(this Color64 color) => color.ToColor32().ToWindowsColor();
+
+        /// <summary>
+        /// Converts a <see cref="PColor64"/> struct to <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color ToWindowsColor(this PColor64 color) => color.ToColor32().ToWindowsColor();
+
+        /// <summary>
+        /// Converts a <see cref="ColorF"/> struct to <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color ToWindowsColor(this ColorF color) => color.ToColor32().ToWindowsColor();
+
+        /// <summary>
+        /// Converts a <see cref="PColorF"/> struct to <a href="https://learn.microsoft.com/en-us/dotnet/api/windows.ui.color" target="_blank">Windows.UI.Color</a>.
+        /// </summary>
+        /// <param name="color">The source color.</param>
+        /// <returns>The result of the conversion.</returns>
+        public static Color ToWindowsColor(this PColorF color) => color.ToColor32().ToWindowsColor();
 
         #endregion
     }
