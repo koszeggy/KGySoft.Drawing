70fafac150a68a0fc6ee07cf6960c2ad244d2233
György Kőszeg <koszeggy@gmail.com>
2023-11-14 14:56:28 +0100

    Drawing.Uwp: Upgrading versions

--- Specific/Uwp/KGySoft.Drawing.Uwp/Properties/AssemblyInfo.cs
+++ Specific/Uwp/KGySoft.Drawing.Uwp/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -29,9 +28,9 @@ using System.Security;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b268a78c-730a-4cb0-8cab-2831c9c935fc")]
 
-[assembly: AssemblyVersion("7.2.0.0")]
-[assembly: AssemblyFileVersion("7.2.0.0")]
-[assembly: AssemblyInformationalVersion("7.2.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
+[assembly: AssemblyInformationalVersion("8.0.0-preview.1")]
 
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: InternalsVisibleTo("KGySoft.Drawing.Uwp.UnitTest, PublicKey=00240000048000009400000006020000002400005253413100040000010001003928BADFAA8C02789566AB7AC64A59DCDE30B798589A68EF92CBB04C9DED3FCBFE41F644D424DCF82F8A13F9148D45EE15785450318388E01AA8C4CF645E81C772E39DCA0D14B33CF48167B70F5C34A0E7B763141ED3AFDDAD0373D9FCD2E153E78D201C5C4EB61DBBD586EC6291EABFBE11879865C3776088605FA8820387C2")]
