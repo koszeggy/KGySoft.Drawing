74efeec993d689d0e3cd8b75abc8f85aa0f276bf
György Kőszeg <koszeggy@gmail.com>
2025-09-08 13:11:51 +0200

    Wpf GeometryExtensions.ToPath: Using WPF internals to convert non-trivial arc segments to Bézier-curves

---
+++ Specific/Wpf/KGySoft.Drawing.Wpf/WinApi/WpfGfx.cs
@@ -0,0 +1,108 @@
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: WpfGfx.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2025 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+#region Used Namespaces
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Windows.Media;
+
+using KGySoft.Reflection;
+
+#endregion
+
+#region Used Aliases
+
+using WpfPoint = System.Windows.Point;
+
+#endregion
+
+#endregion
+
+namespace KGySoft.Drawing.Wpf.WinApi
+{
+    [SecuritySafeCritical]
+    internal static class WpfGfx
+    {
+        #region NativeMethods class
+
+        [SecurityCritical]
+        private static class NativeMethods
+        {
+            #region Constants
+
+#if NET35
+            private const string libName = "wpfgfx_v0300.dll";
+#elif NETFRAMEWORK
+            private const string libName = "wpfgfx_v0400.dll";
+#else
+            private const string libName = "wpfgfx_cor3.dll";
+#endif
+
+            #endregion
+
+            #region Methods
+
+            [DllImport(libName)]
+            internal static extern unsafe void MilUtility_ArcToBezier(
+                WpfPoint ptStart,
+                System.Windows.Size rRadii,
+                double rRotation,
+                bool fLargeArc,
+                SweepDirection fSweepUp,
+                WpfPoint ptEnd,
+                void* pMatrix,
+                WpfPoint* pPt,
+                out int cPieces);
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Methods
+
+        internal static unsafe IList<PointF>? ToBezierPoints(this ArcSegment arcSegment, WpfPoint startPoint)
+        {
+            WpfPoint* bezierPoints = stackalloc WpfPoint[12];
+            int pieceCount;
+            try
+            {
+                NativeMethods.MilUtility_ArcToBezier(startPoint, arcSegment.Size, arcSegment.RotationAngle, arcSegment.IsLargeArc, arcSegment.SweepDirection, arcSegment.Point, null, bezierPoints, out pieceCount);
+            }
+            catch (Exception e) when (!e.IsCritical())
+            {
+                return null;
+            }
+
+            Debug.Assert(pieceCount <= 4);
+            pieceCount = Math.Min(pieceCount, 4);
+            if (pieceCount <= 0)
+                return pieceCount == 0 ? Reflector.EmptyArray<PointF>() : null;
+
+            var result = new List<PointF>(3 * pieceCount + 1) { startPoint.ToPointF() };
+            for (int index = 0; index < pieceCount; ++index)
+                result.AddRange([bezierPoints[3 * index].ToPointF(), bezierPoints[3 * index + 1].ToPointF(), bezierPoints[3 * index + 2].ToPointF()]);
+            return result;
+        }
+
+        #endregion
+    }
+}
--- Specific/Wpf/KGySoft.Drawing.Wpf/_Extensions/GeometryExtensions.cs
+++ Specific/Wpf/KGySoft.Drawing.Wpf/_Extensions/GeometryExtensions.cs
@@ -19,12 +19,14 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media;
 
 using KGySoft.Drawing.Shapes;
+using KGySoft.Drawing.Wpf.WinApi;
 
 #endregion
 
@@ -53,8 +55,40 @@ namespace KGySoft.Drawing.Wpf
         /// <param name="geometry">The <see cref="Geometry"/> instance to convert to a <see cref="Path"/>.</param>
         /// <returns>A <see cref="Path"/> instance that represents the same geometry as the specified <see cref="Geometry"/>.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="geometry"/> is <see langword="null"/>.</exception>
+        [SuppressMessage("ReSharper", "CompareOfFloatsByEqualityOperator", Justification = "Intended")]
         public static Path ToPath(this Geometry geometry)
         {
+            #region Local Methods
+
+            static double ToDegree(double radians) => radians / Math.PI * 180d;
+
+            static (PointF ControlPoint1, PointF ControlPoint2) GetCubicControlPointsFromQuadraticBezier(PointF start, PointF controlPoint, PointF end)
+                => (new PointF(start.X + 2f / 3f * (controlPoint.X - start.X),
+                        start.Y + 2f / 3f * (controlPoint.Y - start.Y)),
+                    new PointF(end.X + 2f / 3f * (controlPoint.X - end.X),
+                        end.Y + 2f / 3f * (controlPoint.Y - end.Y)));
+
+            static WpfPoint GetCenter(WpfPoint startPoint, WpfPoint endPoint, double radiusX, double radiusY, double rotationAngle, bool isLargeArc, bool counterclockwise)
+            {
+                var matrix = new Matrix();
+                matrix.Rotate(-rotationAngle);
+                matrix.Scale(radiusY / radiusX, 1d);
+                startPoint = matrix.Transform(startPoint);
+                endPoint = matrix.Transform(endPoint);
+                WpfPoint midPoint = new((startPoint.X + endPoint.X) / 2d, (startPoint.Y + endPoint.Y) / 2d);
+                Vector startEndDistance = endPoint - startPoint;
+                double halfDistance = startEndDistance.Length / 2d;
+                Vector startEndNormal = isLargeArc != counterclockwise ? new Vector(startEndDistance.Y, -startEndDistance.X) : new Vector(-startEndDistance.Y, startEndDistance.X);
+                if (halfDistance > 0d)
+                    startEndNormal.Normalize();
+                Vector centerOffset = Math.Sqrt(Math.Abs(radiusY * radiusY - halfDistance * halfDistance)) * startEndNormal;
+                WpfPoint center = midPoint + centerOffset;
+                matrix.Invert();
+                return matrix.Transform(center);
+            }
+
+            #endregion
+
             if (geometry == null)
                 throw new ArgumentNullException(nameof(geometry), PublicResources.ArgumentNull);
 
@@ -122,7 +156,7 @@ namespace KGySoft.Drawing.Wpf
 
                             case QuadraticBezierSegment quadraticBezierSegment:
                                 var end = quadraticBezierSegment.Point2.ToPointF();
-                                (PointF cp1, PointF cp2) = GetCubicControlPointsByFromQuadraticBezier(lastPoint, quadraticBezierSegment.Point1.ToPointF(), end);
+                                (PointF cp1, PointF cp2) = GetCubicControlPointsFromQuadraticBezier(lastPoint, quadraticBezierSegment.Point1.ToPointF(), end);
                                 result.AddBezier(lastPoint, cp1, cp2, lastPoint = end);
                                 lastPointAdded = true;
                                 break;
@@ -136,7 +170,7 @@ namespace KGySoft.Drawing.Wpf
                                     for (int i = 0; i < validCount; i += 2)
                                     {
                                         end = points[i + 1].ToPointF();
-                                        (cp1, cp2) = GetCubicControlPointsByFromQuadraticBezier(lastPoint, points[i].ToPointF(), end);
+                                        (cp1, cp2) = GetCubicControlPointsFromQuadraticBezier(lastPoint, points[i].ToPointF(), end);
                                         cubicPoints.AddRange([cp1, cp2, lastPoint = end]);
                                     }
 
@@ -149,14 +183,36 @@ namespace KGySoft.Drawing.Wpf
                             case ArcSegment arcSegment:
                                 var startPoint = lastPoint.ToWpfPoint();
                                 var endPoint = arcSegment.Point;
-                                double width = arcSegment.Size.Width;
-                                double height = arcSegment.Size.Height;
-                                WpfPoint center = GetCenter(startPoint, endPoint, width, height, arcSegment.RotationAngle, arcSegment.IsLargeArc, arcSegment.SweepDirection == SweepDirection.Counterclockwise);
-                                double radStart = Math.Atan2(startPoint.Y - center.Y, startPoint.X - center.X);
-                                double radEnd = Math.Atan2(endPoint.Y - center.Y, endPoint.X - center.X);
 
-                                float startAngle = (float)ToDegree(radStart);
-                                float endAngle = (float)ToDegree(radEnd);
+                                // Elliptical arc with rotation or with non-horizontal/vertical start/end points: attempting to use internal WPF WinAPI in the first place to convert to Bézier segments
+                                if (arcSegment.Size.Width != arcSegment.Size.Height && (arcSegment.RotationAngle != 0d || startPoint.X != endPoint.X && startPoint.Y != endPoint.Y))
+                                {
+                                    IList<PointF>? bezierPoints = arcSegment.ToBezierPoints(startPoint);
+                                    if (bezierPoints != null)
+                                    {
+                                        if (bezierPoints.Count >= 0)
+                                            result.AddBeziers(bezierPoints);
+                                        else
+                                        {
+                                            if (lastPointAdded)
+                                                result.AddPoint(endPoint.ToPointF());
+                                            else
+                                                result.AddLine(lastPoint, endPoint.ToPointF());
+                                        }
+
+                                        lastPoint = arcSegment.Point.ToPointF();
+                                        lastPointAdded = true;
+                                        break;
+                                    }
+                                }
+
+                                // This works correctly only for circles (rotation is irrelevant) and for ellipses without rotation and start/end points on the axes.
+                                // But even if WpfGfx is available, we prefer this for circles and non-rotated ellipses, because for thin paths it produces better results.
+                                double radiusX = arcSegment.Size.Width;
+                                double radiusY = arcSegment.Size.Height;
+                                WpfPoint center = GetCenter(startPoint, endPoint, radiusX, radiusY, arcSegment.RotationAngle, arcSegment.IsLargeArc, arcSegment.SweepDirection == SweepDirection.Counterclockwise);
+                                float startAngle = (float)ToDegree(Math.Atan2(startPoint.Y - center.Y, startPoint.X - center.X));
+                                float endAngle = (float)ToDegree(Math.Atan2(endPoint.Y - center.Y, endPoint.X - center.X));
                                 if (arcSegment.IsLargeArc == Math.Abs(endAngle - startAngle) < 180f)
                                 {
                                     if (startAngle < endAngle)
@@ -168,15 +224,15 @@ namespace KGySoft.Drawing.Wpf
                                 float sweepAngle = endAngle - startAngle;
 
                                 // Fixing the possibly wrong direction of a 180 degree arc. It can occur if one of the arguments of Atan2 is 0.
-                                // ReSharper disable once CompareOfFloatsByEqualityOperator - the tolerance from the double -> float conversion is enough here, and we check a whole number, which can be represented precisely by float
                                 if (Math.Abs(sweepAngle) == 180f && arcSegment.SweepDirection == SweepDirection.Clockwise != sweepAngle > 0f)
                                     sweepAngle = -sweepAngle;
 
                                 if (arcSegment.RotationAngle != 0f)
                                     result.SetTransformation(TransformationMatrix.CreateRotationDegrees((float)arcSegment.RotationAngle, center.ToPointF()));
 
-                                result.AddArc((float)(center.X - width), (float)(center.Y - height), (float)(2d * width), (float)(2d * height), startAngle, sweepAngle);
+                                result.AddArc((float)(center.X - radiusX), (float)(center.Y - radiusY), (float)(2d * radiusX), (float)(2d * radiusY), startAngle, sweepAngle);
                                 result.ResetTransformation();
+
                                 lastPoint = arcSegment.Point.ToPointF();
                                 lastPointAdded = true;
                                 break;
@@ -202,37 +258,6 @@ namespace KGySoft.Drawing.Wpf
         //    if (path.IsEmpty)
         //        return result;
         //}
-
-        #endregion
-
-        #region Private Methods
-
-        private static (PointF ControlPoint1, PointF ControlPoint2) GetCubicControlPointsByFromQuadraticBezier(PointF start, PointF controlPoint, PointF end)
-            => (new PointF(start.X + 2f / 3f * (controlPoint.X - start.X),
-                    start.Y + 2f / 3f * (controlPoint.Y - start.Y)),
-                new PointF(end.X + 2f / 3f * (controlPoint.X - end.X),
-                    end.Y + 2f / 3f * (controlPoint.Y - end.Y)));
-
-        private static double ToDegree(double radians) => radians / Math.PI * 180d;
-
-        private static WpfPoint GetCenter(WpfPoint startPoint, WpfPoint endPoint, double width, double height, double rotationAngle, bool isLargeArc, bool counterclockwise)
-        {
-            Matrix matrix = new Matrix();
-            matrix.Rotate(-rotationAngle);
-            matrix.Scale(height / width, 1d);
-            startPoint = matrix.Transform(startPoint);
-            endPoint = matrix.Transform(endPoint);
-            WpfPoint midPoint = new((startPoint.X + endPoint.X) / 2d, (startPoint.Y + endPoint.Y) / 2d);
-            Vector startEndDistance = endPoint - startPoint;
-            double halfDistance = startEndDistance.Length / 2d;
-            Vector startEndNormal = isLargeArc != counterclockwise ? new Vector(startEndDistance.Y, -startEndDistance.X) : new Vector(-startEndDistance.Y, startEndDistance.X);
-            if (halfDistance > 0d)
-                startEndNormal.Normalize();
-            Vector centerOffset = Math.Sqrt(Math.Abs(height * height - halfDistance * halfDistance)) * startEndNormal;
-            WpfPoint center = midPoint + centerOffset;
-            matrix.Invert();
-            return matrix.Transform(center);
-        }
 
         #endregion
 
